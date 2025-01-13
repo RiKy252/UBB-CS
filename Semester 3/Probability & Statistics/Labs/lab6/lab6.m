64aa3c9a6597089392416b56b4f3d0adf0ac5b3e
Bolog Riccardo <bologriki@gmail.com>
2025-01-13 20:36:33 +0200

    Update lab6.m

--- Semester 3/Probability & Statistics/Labs/lab6/lab6.m
+++ Semester 3/Probability & Statistics/Labs/lab6/lab6.m
@@ -24,17 +24,22 @@ alpha = input("Please give the significance level: ");
 
 % a)
 
-% H0: m = 8.5 <- null hypothesis - system is efficient
+% H0: m >= 8.5 <- null hypothesis - system is efficient
 % H1: m < 8.5 <- alternative hypothesis - system is not efficient
+
 % this is a left-tailed Z-test,
 % for the pop mean when sigma is known
 
 sigma = 5;
 m0 = 8.5; % observed value of the mean
 
+% h is used for rejection
+% p = p-value (probability)
+% ci = confidence interval
+% zobs = z-val = observed value of the statistic
 [h, p, ci, zobs] = ztest(X, m0, sigma, "alpha", alpha, "tail", "left");
 
-% construct the rejection region for this left-tail test
+% construct the rejection region for this left-tail z-test
 z = norminv(alpha, 0, 1);
 RR = [-inf z];
 
@@ -52,6 +57,21 @@ printf("The P-value of our test is %4.3f.\n", p);
 
 % b) ttest -> you get the observed value of the test statistic if
 % you call stats.tstat
+
+[h, p, ci, stats] = ttest(X, m0, "alpha", alpha, "tail", "left");
+
+z = tinv(alpha, n - 1);
+RR = [-inf z];
+
+if h == 1
+  printf("The value of h is %d. The null hypothesis is rejected. Standard is not met\n", h);
+else
+  printf("The value of h is %d. The null hypothesis is not rejected. Standard is met\n", h);
+endif
+
+printf("The rejection region for this t-test is (%4.3f, %4.3f).\n", RR);
+printf("The observed value of the test statistic is %4.3f.\n", stats.tstat);
+printf("The P-value of our test is %4.3f.\n", p);
 
 % 2a) vartest2 -> variances are equal or are different
 % 2b) you choose in the
