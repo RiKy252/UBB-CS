d3517a77798d989354f44184fcde1e5faa380d3a
Bolog Riccardo <bologriki@gmail.com>
2025-01-27 16:47:56 +0200

    Update PracticeSQL.sql

--- Semester 3/Databases/Practice/PracticeSQL.sql
+++ Semester 3/Databases/Practice/PracticeSQL.sql
@@ -19,3 +19,56 @@ select b,c,count(*)
 from S
 group by b,c
 having d <= 1
+
+create table R(
+	RID int primary key,
+	M varchar(50) not null,
+	N varchar(50) not null,
+	P int not null,
+	Q int,
+	O int
+)
+
+insert into R values (10, 'negru', 'toti', 20, 1, 10), (1, 'voda', 'voinici', 21, 1, 5), (2, 'si-a', 'cu', 22, 1, null), (11, 'lui', 'fruntea', 23, 1, null), (3, 'ceata', 'lata', 24, 1, 2)
+
+select * from R
+
+select avg(P)
+from R
+group by P
+having avg(p) > 10
+
+select *
+from R
+where M is null
+
+select M, P from R
+select distinct M, P from R
+
+select * from R
+where Q is not null
+
+select count(r1.RID) NoRows
+from R r1
+where r1.P < ANY (select r3.P
+				  from R r2 full outer join R r3
+				  on r2.RID = r3.RID
+				  group by r3.P)
+
+select *
+from R r1
+where r1.P > any (select r2.P
+			      from R r2)
+
+select avg(r1.P) avgP
+from R r1
+where r1.RID not in (1, 2, 10)
+group by r1.Q
+having avg(r1.P) > any
+(select avg(r2.Q)
+from R r2
+group by r2.P)
+
+select * from R
+
+
